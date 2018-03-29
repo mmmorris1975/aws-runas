5f57a36c56d6a98713b914d24f7963488cddecfa
Mike Morris <michael.m.morris@pearson.com>
2018-03-29 14:24:01 -0500

    add tests from cmd_arg module

---
+++ cmd_arg_test.go
@@ -0,0 +1,19 @@
+package main
+
+import "testing"
+
+func TestIsCumulative(t *testing.T) {
+	c := new(cmdArg)
+	if !c.IsCumulative() {
+		t.Errorf("IsCumulative() is not true")
+	}
+}
+
+func TestSetString(t *testing.T) {
+	c := new(cmdArg)
+	c.Set("val1")
+	c.Set("val2")
+	if len(*c) != 2 {
+		t.Errorf("Expected 2 items, got %d", len(*c))
+	}
+}
