fc9c91c51014829c11c3edd170a0ff15a2b25d9a
Mike Morris <michael.m.morris@pearson.com>
2018-04-03 09:00:11 -0500

    add stub .aws/credentials file

--- spec/runas_role_spec.rb
+++ spec/runas_role_spec.rb
@@ -54,23 +54,23 @@ describe 'tests using a profile with a role' do
 
 
     describe command ('aws-runas -v') do
-          its(:exit_status) { should eq 0 }
-          its(:stdout) { should match /^export AWS_REGION='.*'$/ }
-          its(:stdout) { should match /^export AWS_ACCESS_KEY_ID='ASIA\w+'$/ }
-          its(:stdout) { should match /^export AWS_SECRET_ACCESS_KEY='.*'$/ }
-          its(:stdout) { should match /^export AWS_SESSION_TOKEN='.*'$/ }
-          its(:stdout) { should match /^export AWS_SECURITY_TOKEN='.*'$/ }
-          its(:stderr) { should match /\s+Found cached session token credentials/ }
-          its(:stderr) { should match /\s+ASSUME ROLE OUTPUT:/ }
-        end
+      its(:exit_status) { should eq 0 }
+      its(:stdout) { should match /^export AWS_REGION='.*'$/ }
+      its(:stdout) { should match /^export AWS_ACCESS_KEY_ID='ASIA\w+'$/ }
+      its(:stdout) { should match /^export AWS_SECRET_ACCESS_KEY='.*'$/ }
+      its(:stdout) { should match /^export AWS_SESSION_TOKEN='.*'$/ }
+      its(:stdout) { should match /^export AWS_SECURITY_TOKEN='.*'$/ }
+      its(:stderr) { should match /\s+Found cached session token credentials/ }
+      its(:stderr) { should match /\s+ASSUME ROLE OUTPUT:/ }
+    end
 
-        describe command ('aws-runas -ve') do
-          its(:exit_status) { should eq 0 }
-          its(:stdout) { should match /^export AWS_REGION='.*'$/ }
-          its(:stderr) { should match /\s+Found cached session token credentials/ }
-          its(:stderr) { should match /^Session credentials will expire on/ }
-          its(:stderr) { should match /\s+ASSUME ROLE OUTPUT:/ }
-        end
+    describe command ('aws-runas -ve') do
+      its(:exit_status) { should eq 0 }
+      its(:stdout) { should match /^export AWS_REGION='.*'$/ }
+      its(:stderr) { should match /\s+Found cached session token credentials/ }
+      its(:stderr) { should match /^Session credentials will expire on/ }
+      its(:stderr) { should match /\s+ASSUME ROLE OUTPUT:/ }
+    end
 
     #    describe command ('aws-runas -vrd 10m') do
     #      its(:exit_status) { should eq 0 }
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,6 +6,7 @@ set :path, 'build:$PATH'
 #RSpec.configure do |config|
 #  config.before(:each) do
     ENV['AWS_CONFIG_FILE']='.aws/config'
+    ENV['AWS_SHARED_CREDENTIALS_FILE'='.aws/credentials'
 #  end
 
 #  config.after(:each) do
