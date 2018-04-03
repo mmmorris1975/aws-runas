4a28ec5cdf1dc892bf61de758a0bdb721b6aa21c
Mike Morris <michael.m.morris@pearson.com>
2018-04-03 08:48:15 -0500

    change env var settings in spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,12 +3,12 @@ require 'serverspec'
 set :backend, :exec
 set :path, 'build:$PATH'
 
-RSpec.configure do |config|
-  config.before(:each) do
+#RSpec.configure do |config|
+#  config.before(:each) do
     ENV['AWS_CONFIG_FILE']='.aws/config'
-  end
+#  end
 
-  config.after(:each) do
-    ENV.delete('AWS_CONFIG_FILE')
-  end
-end
+#  config.after(:each) do
+#    ENV.delete('AWS_CONFIG_FILE')
+#  end
+#end
