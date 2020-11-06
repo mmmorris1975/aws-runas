0a362e09e4840e0e8588b96161a09df2f9f9c9b0
Mike Morris <michael.m.morris@gmail.com>
2020-11-06 10:32:53 -0600

    start rewriting rspec tests

--- spec/iam/runas_iam_spec.rb
+++ spec/iam/runas_iam_spec.rb
@@ -1,148 +1,46 @@
 require 'spec_helper'
 
-describe 'test default profile' do
-    before(:each) do
-      ENV['AWS_PROFILE']='default'
-    end
+# This should hopefully be sufficient to validate that using a profile command arg, or the env variable,
+# is returning the correct info.  Hoping to avoid needing to do similar tests for every little piece
 
-    after(:each) do
-      ENV.delete('AWS_PROFILE')
-    end
-
-    describe command ('aws-runas -v') do
-      its(:exit_status) { should eq 0 }
-      its(:stderr) { should_not match /InvalidParameter: 1 validation error/ }
-    end
-end
-
-describe 'tests using a profile without a role' do
-    before(:each) do
-      ENV['AWS_PROFILE']='circleci'
-    end
-
-    after(:each) do
-      ENV.delete('AWS_PROFILE')
-    end
-
-    describe command ('aws-runas -vl') do
+shared_examples_for 'iam user attributes' do |profile|
+    describe command ("aws-runas -vl #{profile}") do
       its(:exit_status) { should eq 0 }
       its(:stdout) { should match /^Available role ARNs for circleci/ }
       its(:stdout) { should match /^\s+arn:aws:iam::\d+:role\/circleci-role$/ }
     end
 
-    describe command ('aws-runas -vm') do
+    describe command ("aws-runas -v list roles #{profile}") do
+          its(:exit_status) { should eq 0 }
+          its(:stdout) { should match /^Available role ARNs for circleci/ }
+          its(:stdout) { should match /^\s+arn:aws:iam::\d+:role\/circleci-role$/ }
+        end
+
+    describe command ("aws-runas -vm #{profile}") do
       its(:exit_status) { should eq 0 }
       its(:stdout) { should match /^arn:aws:iam::\d+:mfa\/circleci$/ }
     end
 
-    describe command ('aws-runas -vs') do
-      its(:exit_status) { should eq 0 }
-      its(:stdout) { should match /^export AWS_REGION='.+'$/ }
-      its(:stdout) { should match /^export AWS_ACCESS_KEY_ID='ASIA\w+'$/ }
-      its(:stdout) { should match /^export AWS_SECRET_ACCESS_KEY='.*'$/ }
-      its(:stdout) { should match /^export AWS_SESSION_TOKEN='.*'$/ }
-      its(:stdout) { should match /^export AWS_SECURITY_TOKEN='.*'$/ }
-      its(:stderr) { should_not match /\s+ASSUME ROLE OUTPUT:/ }
-    end
+    describe command ("aws-runas -v list mfa #{profile}") do
+          its(:exit_status) { should eq 0 }
+          its(:stdout) { should match /^arn:aws:iam::\d+:mfa\/circleci$/ }
+        end
+end
 
-    describe command ('aws-runas -vse') do
-      its(:exit_status) { should eq 0 }
-      its(:stdout) { should match /^export AWS_REGION='.+'$/ }
-      its(:stderr) { should match /^Credentials will expire on/ }
-    end
+describe 'tests for IAM user attributes' do
+  describe 'using profile command argument' do
+    it_should_behave_like 'iam user attributes', 'circleci'
+  end
 
-    describe command ('aws-runas -vsrd 10m') do
-      its(:exit_status) { should eq 0 }
-      its(:stdout) { should match /^export AWS_REGION='.+'$/ }
-      its(:stdout) { should match /^export AWS_ACCESS_KEY_ID='ASIA\w+'$/ }
-      its(:stdout) { should match /^export AWS_SECRET_ACCESS_KEY='.*'$/ }
-      its(:stdout) { should match /^export AWS_SESSION_TOKEN='.*'$/ }
-      its(:stdout) { should match /^export AWS_SECURITY_TOKEN='.*'$/ }
-      its(:stderr) { should match /\s+Detected expired or unset session token credentials, refreshing/ }
-      its(:stderr) { should match /\s+Session token duration too short/ }
-    end
-
-    describe command ('aws-runas -vsrd 360h') do
-      its(:exit_status) { should eq 0 }
-      its(:stdout) { should match /^export AWS_REGION='.+'$/ }
-      its(:stdout) { should match /^export AWS_ACCESS_KEY_ID='ASIA\w+'$/ }
-      its(:stdout) { should match /^export AWS_SECRET_ACCESS_KEY='.*'$/ }
-      its(:stdout) { should match /^export AWS_SESSION_TOKEN='.*'$/ }
-      its(:stdout) { should match /^export AWS_SECURITY_TOKEN='.*'$/ }
-      its(:stderr) { should match /\s+Detected expired or unset session token credentials, refreshing/ }
-      its(:stderr) { should match /\s+Session token duration too long/ }
-    end
-
-    describe command ('aws-runas -vsrd 1d') do
-      its(:exit_status) { should_not eq 0 }
-      its(:stderr) { should match /\s+unknown unit d in duration 1d/ }
-    end
-
-    describe command ('aws-runas -O json') do
-      its(:exit_status) { should eq 0 }
-      its (:stdout) { should match /{"AccessKeyId":"ASIA.*","SecretAccessKey":".*"/}
-    end
-
-    describe 'and setting duration with too short env var' do
-      before(:each) do
-        ENV['SESSION_TOKEN_DURATION'] = '10m'
+  describe 'using profile environment variable' do
+      before(:all) do
+        ENV['AWS_PROFILE']='circleci'
       end
 
-      after(:each) do
-        ENV.delete('SESSION_TOKEN_DURATION')
+      after(:all) do
+        ENV.delete('AWS_PROFILE')
       end
 
-      describe command ('aws-runas -vsr') do
-        its(:exit_status) { should eq 0 }
-        its(:stdout) { should match /^export AWS_REGION='.+'$/ }
-        its(:stdout) { should match /^export AWS_ACCESS_KEY_ID='ASIA\w+'$/ }
-        its(:stdout) { should match /^export AWS_SECRET_ACCESS_KEY='.*'$/ }
-        its(:stdout) { should match /^export AWS_SESSION_TOKEN='.*'$/ }
-        its(:stdout) { should match /^export AWS_SECURITY_TOKEN='.*'$/ }
-        its(:stderr) { should match /\s+Detected expired or unset session token credentials, refreshing/ }
-        its(:stderr) { should match /\s+Session token duration too short/ }
-      end
-    end
-
-    describe 'and setting duration with too long env var' do
-      before(:each) do
-        ENV['SESSION_TOKEN_DURATION'] = '360h'
-      end
-
-      after(:each) do
-        ENV.delete('SESSION_TOKEN_DURATION')
-      end
-
-      describe command ('aws-runas -vsr') do
-        its(:exit_status) { should eq 0 }
-        its(:stdout) { should match /^export AWS_REGION='.+'$/ }
-        its(:stdout) { should match /^export AWS_ACCESS_KEY_ID='ASIA\w+'$/ }
-        its(:stdout) { should match /^export AWS_SECRET_ACCESS_KEY='.*'$/ }
-        its(:stdout) { should match /^export AWS_SESSION_TOKEN='.*'$/ }
-        its(:stdout) { should match /^export AWS_SECURITY_TOKEN='.*'$/ }
-        its(:stderr) { should match /\s+Detected expired or unset session token credentials, refreshing/ }
-        its(:stderr) { should match /\s+Session token duration too long/ }
-      end
-    end
-
-    describe 'and setting duration with invalid env var' do
-      before(:each) do
-        ENV['SESSION_TOKEN_DURATION'] = '1d'
-      end
-
-      after(:each) do
-        ENV.delete('SESSION_TOKEN_DURATION')
-      end
-
-      describe command ('aws-runas -vsr') do
-        its(:exit_status) { should_not eq 0 }
-        its(:stderr) { should match /\s+unknown unit d in duration 1d/ }
-      end
-    end
-
-    describe command('aws-runas --whoami') do
-        its(:exit_status) { should eq 0 }
-        its(:stderr) { should match /^\s+Account: "686784119290",$/}
-        its(:stderr) { should match /^\s+Arn: "arn:aws:iam::686784119290:user\/circleci",$/}
-    end
+      it_should_behave_like 'iam user attributes'
+  end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,3 +4,4 @@ set :backend, :exec
 set :path, 'build:$PATH'
 
 ENV['AWS_CONFIG_FILE']='.aws/config'
+ENV['AWS_REGION'] = 'us-east-2'
