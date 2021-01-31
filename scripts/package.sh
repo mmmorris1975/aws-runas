09f31c29469a775becb193c6e3a85a99e2f1c368
Mike Morris <michael.m.morris@gmail.com>
2021-01-31 16:54:50 -0600

    glam up the linux packaging script
    add new prereq for fpm
    install bash completion to system-wide location
    add all linux shell completion scripts to /usr/local/share

--- scripts/package.sh
+++ scripts/package.sh
@@ -1,22 +1,30 @@
 #!/bin/bash -e
 
+PKG_DIR=/var/tmp/pkg
 BUILD_DIR=`realpath $(dirname $0)/..`
 
 cd $BUILD_DIR
+mkdir -p $PKG_DIR/etc/bash_completion.d
+mkdir -p $PKG_DIR/usr/local/bin $PKG_DIR/usr/local/share/aws-runas
 
-apt-get update && apt-get -y install build-essential alien ruby-dev rubygems
+apt-get update && apt-get -y install build-essential alien ruby-dev rubygems git
 gem install --no-ri --no-rdoc fpm
 
-cp $BUILD_DIR/aws-runas-*-linux-amd64 /var/tmp/aws-runas
+cp $BUILD_DIR/aws-runas-*-linux-amd64 ${PKG_DIR}/usr/local/bin/aws-runas
+cp $BUILD_DIR/extras/aws-runas-*-completion ${PKG_DIR}/usr/local/share/aws-runas/
+cp $BUILD_DIR/extras/aws-runas-bash-completion ${PKG_DIR}/etc/bash_completion.d/aws-runas
+
+chmod a+x ${PKG_DIR}/usr/local/bin/aws-runas
+chmod a+r ${PKG_DIR}/usr/local/share/aws-runas/* ${PKG_DIR}/etc/bash_completion.d/aws-runas
 
 # Build RPM package
 fpm --verbose -s dir -t rpm --name aws-runas --version $VER --license MIT --architecture 'x86_64' --provides 'aws-runas' \
   --description 'aws-runas' --url 'https://github.com/mmmorris1975/aws-runas' --maintainer 'mmmorris1975@github' \
   --rpm-user bin --rpm-group bin --rpm-digest sha1 --after-install $BUILD_DIR/scripts/pkg-post-install.sh \
-  --depends libcap --prefix /usr/local/bin -C /var/tmp aws-runas
+  --depends libcap --prefix / -C $PKG_DIR
 
 # Build DEB (Debian's "alien" package conversion tool didn't seem to preserve the RPM post-install script)
 fpm --verbose -s dir -t deb --name aws-runas --version $VER --license MIT --architecture 'x86_64' --provides 'aws-runas' \
   --description 'aws-runas' --url 'https://github.com/mmmorris1975/aws-runas' --maintainer 'mmmorris1975@github' \
   --deb-user bin --deb-group bin --after-install $BUILD_DIR/scripts/pkg-post-install.sh \
-  --deb-pre-depends libcap2-bin --prefix /usr/local/bin -C /var/tmp aws-runas
+  --deb-pre-depends libcap2-bin --prefix / -C $PKG_DIR
