145a6008cd5673e28a20998038be6863bbfc3639
Mike Morris <michael.m.morris@gmail.com>
2021-02-02 11:04:26 -0600

    explicitly set project name in release script

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 
-SCRIPTDIR=$(realpath $(dirname $0)/..)
-NAME=$(basename $SCRIPTDIR)
-
 # Only release if the commit is associated with a tag (maybe only matches a semver-like tag?)
 git describe --tags --exact-match --first-parent || exit 0
 VER=$(git describe --tags --exact-match --first-parent)
@@ -11,6 +8,8 @@ wget -O - https://github.com/cli/cli/releases/download/v1.4.0/gh_1.4.0_linux_amd
 GH=$(find /var/tmp/gh* -name gh)
 
 cd ${1:-.}
+
+NAME="aws-runas"
 
 sha256sum aws-runas*.zip aws-runas*.deb aws-runas*.rpm >${NAME}_${VER}.sha256sum
 cat *.sha256sum
