540bc36a93d6b653d67cb752c03d561b6b0fb783
Mike Morris <michael.m.morris@gmail.com>
2021-03-18 11:01:53 -0500

    copyright notices ... copyright notices everywhere

--- credentials/helpers/mfa_token_provider.go
+++ credentials/helpers/mfa_token_provider.go
@@ -1,3 +1,16 @@
+/*
+ * Copyright (c) 2021 Michael Morris. All Rights Reserved.
+ *
+ * Licensed under the MIT license (the "License"). You may not use this file except in compliance
+ * with the License. A copy of the License is located at
+ *
+ * https://github.com/mmmorris1975/aws-runas/blob/master/LICENSE
+ *
+ * or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+ * for the specific language governing permissions and limitations under the License.
+ */
+
 package helpers
 
 import (
@@ -21,7 +34,7 @@ func NewMfaTokenProvider(in io.Reader) *mfaTokenProvider {
 func (p *mfaTokenProvider) ReadInput() (string, error) {
 	var val string
 
-	fmt.Fprint(os.Stderr, "MFA token code: ")
+	_, _ = fmt.Fprint(os.Stderr, "MFA token code: ")
 	_, err := fmt.Fscanln(p.input, &val)
 	if err != nil && err != io.EOF {
 		return "", err
--- spec/iam/runas_iam_spec.rb
+++ spec/iam/runas_iam_spec.rb
@@ -1,3 +1,16 @@
+#
+# Copyright (c) 2021 Michael Morris. All Rights Reserved.
+#
+# Licensed under the MIT license (the "License"). You may not use this file except in compliance
+# with the License. A copy of the License is located at
+#
+# https://github.com/mmmorris1975/aws-runas/blob/master/LICENSE
+#
+# or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+# for the specific language governing permissions and limitations under the License.
+#
+
 require 'spec_helper'
 
 # This should hopefully be sufficient to validate that using a profile command arg, or the env variable,
