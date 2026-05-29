ade198b0cfb4f630d7bf34c3b4459142ca323ceb
Mike Morris <michael.m.morris@gmail.com>
2026-05-28 23:33:06 -0500

    appease the linter

--- client/external/client_factory.go
+++ client/external/client_factory.go
@@ -79,7 +79,7 @@ func MustGetWebIdentityClient(provider, authUrl string, cfg OidcClientConfig) We
 
 // the switch statement will continue to grow as new providers are added, so keep the linter quiet
 //
-//nolint:gocyclo,funlen
+//nolint:gocyclo,funlen,gocognit
 func lookupClient(provider, authUrl string, cfg OidcClientConfig) (any, error) {
 	if len(provider) < 1 {
 		provider = divineClient(authUrl, http.MethodHead)
