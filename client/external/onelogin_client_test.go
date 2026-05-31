5314934cf08db19ea194ac48b90233fa048c4123
Mike Morris <michael.m.morris@gmail.com>
2026-05-31 07:47:56 -0500

    enhance SamlAssertion test coverage

--- client/external/onelogin_client_test.go
+++ client/external/onelogin_client_test.go
@@ -43,6 +43,7 @@ func init() {
 	mux.HandleFunc("/api/1/login/auth", olAuthHandler)
 	mux.HandleFunc("/api/2/mfa/users/", olMfaDeviceHandler)
 	mux.HandleFunc("/api/2/saml_assertion", olSamlAssertionHandler)
+	mux.HandleFunc("/api/2/saml_assertion/verify_factor", olSamlVerifyMfaHandler)
 	mux.HandleFunc("/verify_mfa_local", olVerifyMfaHandler)
 
 	oneloginMock = httptest.NewServer(mux)
@@ -265,6 +266,19 @@ func TestOneloginClient_IdentityToken(t *testing.T) {
 }
 
 func TestOneloginClient_SamlAssertion(t *testing.T) {
+	t.Run("bad gather creds", func(t *testing.T) {
+		c := newMockOneloginClient()
+		c.CredentialInputProvider = func(user, password string) (string, string, error) {
+			return "", "", errors.New("error time")
+		}
+
+		if _, err := c.SamlAssertion(); err == nil {
+			t.Error("did not receive expected error")
+		}
+	})
+}
+
+func TestOneloginClient_SamlAssertion_Plain(t *testing.T) {
 	c := newMockOneloginClient()
 	c.Username = "testuser"
 	c.Password = "goodPassword"
@@ -303,6 +317,70 @@ func TestOneloginClient_SamlAssertion(t *testing.T) {
 		if r == nil || len(*r) < 1 {
 			t.Error("data mismatch")
 			return
+		}
+	})
+}
+
+func TestOneloginClient_SamlAssertion_CodeMfa(t *testing.T) {
+	t.Run("explicit configuration", func(t *testing.T) {
+		c := newMockOneloginClient()
+		c.Username = "samlcodemfa"
+		c.Password = "goodPassword"
+		c.MfaType = "code"
+		c.MfaTokenProvider = func() (string, error) {
+			return "54321", nil
+		}
+
+		if _, err := c.SamlAssertion(); err != nil {
+			t.Error(err)
+		}
+	})
+
+	t.Run("discovered factor", func(t *testing.T) {
+		c := newMockOneloginClient()
+		c.Username = "samlcodemfa"
+		c.Password = "goodPassword"
+		c.MfaTokenCode = "543210"
+		c.MfaTokenProvider = func() (string, error) {
+			return "54321", nil
+		}
+
+		if _, err := c.SamlAssertion(); err != nil {
+			t.Error(err)
+		}
+	})
+
+	t.Run("no factor found", func(t *testing.T) {
+		c := newMockOneloginClient()
+		c.Username = "samlcodemfa"
+		c.Password = "goodPassword"
+		c.MfaType = "none"
+
+		if _, err := c.SamlAssertion(); err == nil {
+			t.Error("did not receive expected error")
+		}
+	})
+}
+
+func TestOneloginClient_SamlAssertion_PushMfa(t *testing.T) {
+	t.Run("explicit configuration", func(t *testing.T) {
+		c := newMockOneloginClient()
+		c.Username = "samlpushmfa"
+		c.Password = "goodPassword"
+		c.MfaType = "push"
+
+		if _, err := c.SamlAssertion(); err != nil {
+			t.Error(err)
+		}
+	})
+
+	t.Run("discovered factor", func(t *testing.T) {
+		c := newMockOneloginClient()
+		c.Username = "samlpushmfa"
+		c.Password = "goodPassword"
+
+		if _, err := c.SamlAssertion(); err != nil {
+			t.Error(err)
 		}
 	})
 }
@@ -419,6 +497,7 @@ func olSamlHandler(w http.ResponseWriter, r *http.Request) {
 	_, _ = fmt.Fprintf(w, body, r.Host)
 }
 
+// TODO - enhance handler to test MFA cases, since it is now separate from the Authenticate() path.  Use olAuthHandler() for guidance
 func olSamlAssertionHandler(w http.ResponseWriter, r *http.Request) {
 	defer r.Body.Close()
 	body, err := io.ReadAll(r.Body)
@@ -434,16 +513,95 @@ func olSamlAssertionHandler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	w.Header().Set("Content-Type", "application/json")
-	if params["username_or_email"] == "" {
-		data, _ := json.Marshal(&oneloginApiErrorV2{Status: http.StatusUnauthorized, Message: "Authentication failed"})
+
+	if params["password"] != "goodPassword" {
+		data, _ := json.Marshal(&oneloginApiErrorV2{Status: http.StatusUnauthorized, Message: "Authentication Failed: Invalid user credentials"})
 		http.Error(w, string(data), http.StatusUnauthorized)
 		return
 	}
 
-	//nolint:lll
-	const samlData = "PHNhbWw6QXR0cmlidXRlU3RhdGVtZW50PjxzYW1sOkF0dHJpYnV0ZSBOYW1lPSJodHRwczovL2F3cy5hbWF6b24uY29tL1NBTUwvQXR0cmlidXRlcy9Sb2xlU2Vzc2lvbk5hbWUiPjxzYW1sOkF0dHJpYnV0ZVZhbHVlIHhtbG5zOnhzPSJodHRwOi8vd3d3LnczLm9yZy8yMDAxL1hNTFNjaGVtYSIgeG1sbnM6eHNpPSJodHRwOi8vd3d3LnczLm9yZy8yMDAxL1hNTFNjaGVtYS1pbnN0YW5jZSIgeHNpOnR5cGU9InhzOnN0cmluZyI+bXktc2FtbC11c2VyPC9zYW1sOkF0dHJpYnV0ZVZhbHVlPjwvc2FtbDpBdHRyaWJ1dGU+PHNhbWw6QXR0cmlidXRlIE5hbWU9Imh0dHBzOi8vYXdzLmFtYXpvbi5jb20vU0FNTC9BdHRyaWJ1dGVzL1Nlc3Npb25EdXJhdGlvbiI+PHNhbWw6QXR0cmlidXRlVmFsdWUgeG1sbnM6eHM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDEvWE1MU2NoZW1hIiB4bWxuczp4c2k9Imh0dHA6Ly93d3cudzMub3JnLzIwMDEvWE1MU2NoZW1hLWluc3RhbmNlIiB4c2k6dHlwZT0ieHM6c3RyaW5nIj40MzIwMDwvc2FtbDpBdHRyaWJ1dGVWYWx1ZT48L3NhbWw6QXR0cmlidXRlPjxzYW1sOkF0dHJpYnV0ZSBOYW1lPSJ1cm46b2lkOjEuMy42LjEuNC4xLjU5MjMuMS4xLjEuMTEiPjxzYW1sOkF0dHJpYnV0ZVZhbHVlIHhtbG5zOnhzPSJodHRwOi8vd3d3LnczLm9yZy8yMDAxL1hNTFNjaGVtYSIgeG1sbnM6eHNpPSJodHRwOi8vd3d3LnczLm9yZy8yMDAxL1hNTFNjaGVtYS1pbnN0YW5jZSIgeHNpOnR5cGU9InhzOnN0cmluZyI+Mjwvc2FtbDpBdHRyaWJ1dGVWYWx1ZT48L3NhbWw6QXR0cmlidXRlPjxzYW1sOkF0dHJpYnV0ZSBOYW1lPSJodHRwczovL2F3cy5hbWF6b24uY29tL1NBTUwvQXR0cmlidXRlcy9Sb2xlIj48c2FtbDpBdHRyaWJ1dGVWYWx1ZSB4bWxuczp4cz0iaHR0cDovL3d3dy53My5vcmcvMjAwMS9YTUxTY2hlbWEiIHhtbG5zOnhzaT0iaHR0cDovL3d3dy53My5vcmcvMjAwMS9YTUxTY2hlbWEtaW5zdGFuY2UiIHhzaTp0eXBlPSJ4czpzdHJpbmciPmFybjphd3M6aWFtOjoxMjM0NTY3ODkwOnJvbGUvUG93ZXJVc2VyLGFybjphd3M6aWFtOjoxMjM0NTY3ODkwOnNhbWwtcHJvdmlkZXIvbXlTU088L3NhbWw6QXR0cmlidXRlVmFsdWU+PHNhbWw6QXR0cmlidXRlVmFsdWUgeG1sbnM6eHM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDEvWE1MU2NoZW1hIiB4bWxuczp4c2k9Imh0dHA6Ly93d3cudzMub3JnLzIwMDEvWE1MU2NoZW1hLWluc3RhbmNlIiB4c2k6dHlwZT0ieHM6c3RyaW5nIj5hcm46YXdzOmlhbTo6MDk4NzY1NDMyMTpyb2xlL1Bvd2VyVXNlcixhcm46YXdzOmlhbTo6MDk4NzY1NDMyMTpzYW1sLXByb3ZpZGVyL215U1NPPC9zYW1sOkF0dHJpYnV0ZVZhbHVlPjxzYW1sOkF0dHJpYnV0ZVZhbHVlIHhtbG5zOnhzPSJodHRwOi8vd3d3LnczLm9yZy8yMDAxL1hNTFNjaGVtYSIgeG1sbnM6eHNpPSJodHRwOi8vd3d3LnczLm9yZy8yMDAxL1hNTFNjaGVtYS1pbnN0YW5jZSIgeHNpOnR5cGU9InhzOnN0cmluZyI+YXJuOmF3czppYW06OjExMTExMTExMTpyb2xlL0FkbWluLGFybjphd3M6aWFtOjoxMTExMTExMTE6c2FtbC1wcm92aWRlci9teVNTTzwvc2FtbDpBdHRyaWJ1dGVWYWx1ZT48c2FtbDpBdHRyaWJ1dGVWYWx1ZSB4bWxuczp4cz0iaHR0cDovL3d3dy53My5vcmcvMjAwMS9YTUxTY2hlbWEiIHhtbG5zOnhzaT0iaHR0cDovL3d3dy53My5vcmcvMjAwMS9YTUxTY2hlbWEtaW5zdGFuY2UiIHhzaTp0eXBlPSJ4czpzdHJpbmciPmFybjphd3M6aWFtOjoyMjIyMjIyMjI6cm9sZS9tYW5hZ2VkLXJvbGUvQWRtaW4sYXJuOmF3czppYW06OjIyMjIyMjIyMjpzYW1sLXByb3ZpZGVyL215U1NPPC9zYW1sOkF0dHJpYnV0ZVZhbHVlPjxzYW1sOkF0dHJpYnV0ZVZhbHVlIHhtbG5zOnhzPSJodHRwOi8vd3d3LnczLm9yZy8yMDAxL1hNTFNjaGVtYSIgeG1sbnM6eHNpPSJodHRwOi8vd3d3LnczLm9yZy8yMDAxL1hNTFNjaGVtYS1pbnN0YW5jZSIgeHNpOnR5cGU9InhzOnN0cmluZyI+YXJuOmF3czppYW06OjMzMzMzMzMzMzpyb2xlL0FkbWluLGFybjphd3M6aWFtOjozMzMzMzMzMzM6c2FtbC1wcm92aWRlci9teVNTTzwvc2FtbDpBdHRyaWJ1dGVWYWx1ZT48L3NhbWw6QXR0cmlidXRlPjwvc2FtbDpBdHRyaWJ1dGVTdGF0ZW1lbnQ+Cg=="
-	data, _ := json.Marshal(&oneloginAuthReplyV2{Data: samlData, Message: "success"})
-	_, _ = w.Write(data)
+	cbUrl := fmt.Sprintf("http://%s/api/2/saml_assertion/verify_factor", r.Host)
+
+	switch params["username_or_email"] {
+	case "samlcodemfa":
+		data, _ := json.Marshal(&oneloginSamlReply{
+			Message: "MFA is required for this user",
+			oneloginAuthData: oneloginAuthData{
+				StateToken:  "saml state token",
+				CallbackUrl: cbUrl,
+				Devices:     []*oneloginMfaDevice{{DeviceType: "Google Authenticator", DeviceId: 123}},
+				User:        &oneloginUser{Id: 123},
+			},
+		})
+		_, _ = w.Write(data)
+	case "samlpushmfa":
+		data, _ := json.Marshal(&oneloginSamlReply{
+			Message: "MFA is required for this user",
+			oneloginAuthData: oneloginAuthData{
+				StateToken:  "saml state token",
+				CallbackUrl: cbUrl,
+				Devices:     []*oneloginMfaDevice{{DeviceType: "OneLogin Protect", DeviceId: 321}},
+				User:        &oneloginUser{Id: 321},
+			},
+		})
+		_, _ = w.Write(data)
+	default:
+		//nolint:lll
+		const samlData = "PHNhbWw6QXR0cmlidXRlU3RhdGVtZW50PjxzYW1sOkF0dHJpYnV0ZSBOYW1lPSJodHRwczovL2F3cy5hbWF6b24uY29tL1NBTUwvQXR0cmlidXRlcy9Sb2xlU2Vzc2lvbk5hbWUiPjxzYW1sOkF0dHJpYnV0ZVZhbHVlIHhtbG5zOnhzPSJodHRwOi8vd3d3LnczLm9yZy8yMDAxL1hNTFNjaGVtYSIgeG1sbnM6eHNpPSJodHRwOi8vd3d3LnczLm9yZy8yMDAxL1hNTFNjaGVtYS1pbnN0YW5jZSIgeHNpOnR5cGU9InhzOnN0cmluZyI+bXktc2FtbC11c2VyPC9zYW1sOkF0dHJpYnV0ZVZhbHVlPjwvc2FtbDpBdHRyaWJ1dGU+PHNhbWw6QXR0cmlidXRlIE5hbWU9Imh0dHBzOi8vYXdzLmFtYXpvbi5jb20vU0FNTC9BdHRyaWJ1dGVzL1Nlc3Npb25EdXJhdGlvbiI+PHNhbWw6QXR0cmlidXRlVmFsdWUgeG1sbnM6eHM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDEvWE1MU2NoZW1hIiB4bWxuczp4c2k9Imh0dHA6Ly93d3cudzMub3JnLzIwMDEvWE1MU2NoZW1hLWluc3RhbmNlIiB4c2k6dHlwZT0ieHM6c3RyaW5nIj40MzIwMDwvc2FtbDpBdHRyaWJ1dGVWYWx1ZT48L3NhbWw6QXR0cmlidXRlPjxzYW1sOkF0dHJpYnV0ZSBOYW1lPSJ1cm46b2lkOjEuMy42LjEuNC4xLjU5MjMuMS4xLjEuMTEiPjxzYW1sOkF0dHJpYnV0ZVZhbHVlIHhtbG5zOnhzPSJodHRwOi8vd3d3LnczLm9yZy8yMDAxL1hNTFNjaGVtYSIgeG1sbnM6eHNpPSJodHRwOi8vd3d3LnczLm9yZy8yMDAxL1hNTFNjaGVtYS1pbnN0YW5jZSIgeHNpOnR5cGU9InhzOnN0cmluZyI+Mjwvc2FtbDpBdHRyaWJ1dGVWYWx1ZT48L3NhbWw6QXR0cmlidXRlPjxzYW1sOkF0dHJpYnV0ZSBOYW1lPSJodHRwczovL2F3cy5hbWF6b24uY29tL1NBTUwvQXR0cmlidXRlcy9Sb2xlIj48c2FtbDpBdHRyaWJ1dGVWYWx1ZSB4bWxuczp4cz0iaHR0cDovL3d3dy53My5vcmcvMjAwMS9YTUxTY2hlbWEiIHhtbG5zOnhzaT0iaHR0cDovL3d3dy53My5vcmcvMjAwMS9YTUxTY2hlbWEtaW5zdGFuY2UiIHhzaTp0eXBlPSJ4czpzdHJpbmciPmFybjphd3M6aWFtOjoxMjM0NTY3ODkwOnJvbGUvUG93ZXJVc2VyLGFybjphd3M6aWFtOjoxMjM0NTY3ODkwOnNhbWwtcHJvdmlkZXIvbXlTU088L3NhbWw6QXR0cmlidXRlVmFsdWU+PHNhbWw6QXR0cmlidXRlVmFsdWUgeG1sbnM6eHM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDEvWE1MU2NoZW1hIiB4bWxuczp4c2k9Imh0dHA6Ly93d3cudzMub3JnLzIwMDEvWE1MU2NoZW1hLWluc3RhbmNlIiB4c2k6dHlwZT0ieHM6c3RyaW5nIj5hcm46YXdzOmlhbTo6MDk4NzY1NDMyMTpyb2xlL1Bvd2VyVXNlcixhcm46YXdzOmlhbTo6MDk4NzY1NDMyMTpzYW1sLXByb3ZpZGVyL215U1NPPC9zYW1sOkF0dHJpYnV0ZVZhbHVlPjxzYW1sOkF0dHJpYnV0ZVZhbHVlIHhtbG5zOnhzPSJodHRwOi8vd3d3LnczLm9yZy8yMDAxL1hNTFNjaGVtYSIgeG1sbnM6eHNpPSJodHRwOi8vd3d3LnczLm9yZy8yMDAxL1hNTFNjaGVtYS1pbnN0YW5jZSIgeHNpOnR5cGU9InhzOnN0cmluZyI+YXJuOmF3czppYW06OjExMTExMTExMTpyb2xlL0FkbWluLGFybjphd3M6aWFtOjoxMTExMTExMTE6c2FtbC1wcm92aWRlci9teVNTTzwvc2FtbDpBdHRyaWJ1dGVWYWx1ZT48c2FtbDpBdHRyaWJ1dGVWYWx1ZSB4bWxuczp4cz0iaHR0cDovL3d3dy53My5vcmcvMjAwMS9YTUxTY2hlbWEiIHhtbG5zOnhzaT0iaHR0cDovL3d3dy53My5vcmcvMjAwMS9YTUxTY2hlbWEtaW5zdGFuY2UiIHhzaTp0eXBlPSJ4czpzdHJpbmciPmFybjphd3M6aWFtOjoyMjIyMjIyMjI6cm9sZS9tYW5hZ2VkLXJvbGUvQWRtaW4sYXJuOmF3czppYW06OjIyMjIyMjIyMjpzYW1sLXByb3ZpZGVyL215U1NPPC9zYW1sOkF0dHJpYnV0ZVZhbHVlPjxzYW1sOkF0dHJpYnV0ZVZhbHVlIHhtbG5zOnhzPSJodHRwOi8vd3d3LnczLm9yZy8yMDAxL1hNTFNjaGVtYSIgeG1sbnM6eHNpPSJodHRwOi8vd3d3LnczLm9yZy8yMDAxL1hNTFNjaGVtYS1pbnN0YW5jZSIgeHNpOnR5cGU9InhzOnN0cmluZyI+YXJuOmF3czppYW06OjMzMzMzMzMzMzpyb2xlL0FkbWluLGFybjphd3M6aWFtOjozMzMzMzMzMzM6c2FtbC1wcm92aWRlci9teVNTTzwvc2FtbDpBdHRyaWJ1dGVWYWx1ZT48L3NhbWw6QXR0cmlidXRlPjwvc2FtbDpBdHRyaWJ1dGVTdGF0ZW1lbnQ+Cg=="
+		data, _ := json.Marshal(&oneloginAuthReplyV2{Data: samlData, Message: "success"})
+		_, _ = w.Write(data)
+	}
+}
+
+func olSamlVerifyMfaHandler(w http.ResponseWriter, r *http.Request) {
+	defer r.Body.Close()
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	verifyReq := new(oneloginVerifyFactorRequest)
+	if err := json.Unmarshal(body, verifyReq); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	switch vr := verifyReq; {
+	case vr.DeviceId == "555":
+		// code mfa
+		if vr.OtpToken == "54321" {
+			data, _ := json.Marshal(&oneloginAuthReplyV2{
+				Message: "success",
+				Data:    "token",
+			})
+
+			_, _ = w.Write(data)
+			return
+		}
+		data, _ := json.Marshal(&oneloginApiErrorV2{
+			Status:  http.StatusUnauthorized,
+			Message: "Failed authentication with this factor",
+		})
+		http.Error(w, string(data), http.StatusUnauthorized)
+	case vr.DeviceId == "666":
+		// push mfa — success on seconds divisible by 6, pending otherwise
+		reply := &oneloginAuthReplyV2{
+			Message: "Authentication pending",
+			Data: "",
+		}
+
+		if time.Now().Second()%6 == 0 {
+			reply.Message = "success"
+			reply.Data = "session token"
+		}
+
+		data, _ := json.Marshal(reply)
+		_, _ = w.Write(data)
+	default:
+		http.Error(w, "unknown mfa failure", http.StatusUnauthorized)
+	}
 }
 
 func olSessionHandler(w http.ResponseWriter, r *http.Request) {
@@ -648,12 +806,12 @@ func olVerifyMfaHandler(w http.ResponseWriter, r *http.Request) {
 		})
 		http.Error(w, string(data), http.StatusUnauthorized)
 	case vr.DeviceId == "666":
-		// push mfa — success on seconds divisible by 10, pending otherwise
+		// push mfa — success on seconds divisible by 6, pending otherwise
 		reply := &oneloginAuthReply{
 			Status: &oneloginApiStatus{Code: http.StatusOK, Message: "Authentication pending"},
 		}
 
-		if time.Now().Second()%10 == 0 {
+		if time.Now().Second()%6 == 0 {
 			reply.Status.Message = "success"
 			reply.Data = []*oneloginAuthData{{SessionToken: "session token"}}
 		}
